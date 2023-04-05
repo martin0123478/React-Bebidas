21a3b7bd9e6ff666ecc41f3ec77664b0ef00b3be
martin0123478 <martin_agui97@outlook.es>
2023-04-05 17:51:42 -0600

    feat: show ingredients of drinks

--- src/components/ModalBebida.jsx
+++ src/components/ModalBebida.jsx
@@ -1,14 +1,39 @@
 import {Modal,Image} from 'react-bootstrap'
 import useBebidas from '../hooks/useBebidas'
 const ModalBebida = () => {
-  const {modal,handleModalClick} = useBebidas()
+  const {modal,handleModalClick,receta} = useBebidas()
    
+const mostrarIngredientes = () =>{
+  let ingredientes=[]
 
+  for(let i=1;i<16;i++){
+    if(receta[`strIngredient${i}`]){
+      ingredientes.push(
+        <li>{receta[`strIngredient${i}`]} {receta[`strMeasure${i}`]}</li>
+      )
+    }
+    return ingredientes
+  }
+
+}
   return (
     <Modal show={modal} onHide={handleModalClick}>
-
+    <Image
+        src={receta.strDrinkThumb}
+        alt={`Imagen receta ${receta.strDrink}`}
+        />
+        <Modal.Header>
+          <Modal.Title>{receta.strDrink}</Modal.Title>
+        </Modal.Header>
+  
         <Modal.Body>
-            Cuerpo de Modal
+            <div className='p-3'>
+              <h2>Instrucciones</h2>
+              {receta.strInstructions}
+              <h2>Ingredientes y Cantidad</h2>
+              {mostrarIngredientes()}
+
+            </div>
         </Modal.Body>
     </Modal>
   )
